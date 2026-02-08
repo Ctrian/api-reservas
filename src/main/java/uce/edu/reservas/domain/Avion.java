b77f6eb4be129b23b32ff5e0a590009c11d7d25d
Ctrian <andrescalvache47@gmail.com>
2026-02-07 23:19:02 -0500

    debug con drop-and-create para la base de datos

--- src/main/java/uce/edu/reservas/domain/Avion.java
+++ src/main/java/uce/edu/reservas/domain/Avion.java
@@ -6,7 +6,6 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
